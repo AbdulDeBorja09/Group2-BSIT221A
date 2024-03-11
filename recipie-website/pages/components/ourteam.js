ba94c70308f5aab1d966627b29848042eed49399
AbdulDeBorja09 <abduldb09@gmail.com>
2024-03-11 19:15:36 +0800

    Fix about us flex

--- recipie-website/pages/components/ourteam.js
+++ recipie-website/pages/components/ourteam.js
@@ -3,7 +3,7 @@ import styles from "@/styles/style.module.scss";
 export default function Ourteam() {
   return (
     <>
-      <div className={styles.about_image_flex} class="about_image_flex">
+      <div className={styles.about_image_flex} style={{ display: "flex" }}>
         <img className={styles.imagebox} src={`/profiles/abdul.png`} />
         <img className={styles.imagebox} src={`/profiles/ethan.png`} />
         <img className={styles.imagebox} src={`/profiles/Khylle.png`} />
