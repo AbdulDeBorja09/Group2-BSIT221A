63bcc23432d00963449b25239ce41993561f3ee0
AbdulDeBorja09 <abduldb09@gmail.com>
2024-03-11 20:55:15 +0800

    Fix 404 page

--- recipie-website/pages/404.js
+++ recipie-website/pages/404.js
@@ -1,30 +1,31 @@
 import Link from "next/link";
 import styles from "@/styles/style.module.scss";
+
 export default function Custom404() {
-    return (
-        <>
-            <section className={styles.error_page}>
-                <div className={styles.top}>
-                    <div className={styles.image}>
-                        <h1 className={styles.error}>404</h1>
-                        <img className={styles.chef} src="../404.png" alt="" />
-                    </div>
-                    <div className={styles.text}>
-                        <h2 className={styles.error_msg}>
-                            LOOKS LIKE YOU GOT LOST IN THE KITCHEN
-                        </h2>
-                    </div>
-                </div>
-                <div className={styles.bottom}>
-                    <p className={styles.error_msg}>
-                        The page you are looking for might be removed or temporary
-                        unavailable
-                    </p>
-                    <Link href="../" className={styles.return_btn}>
-                        GO BACK TO HOME PAGE
-                    </Link>
-                </div>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section className={styles.error_page}>
+        <div className={styles.top}>
+          <div className={styles.images}>
+            <h1 className={styles.error}>404</h1>
+            <img className={styles.chef} src="../404.png" alt="" />
+          </div>
+          <div className={styles.text}>
+            <h2 className={styles.error_msg}>
+              LOOKS LIKE YOU GOT LOST IN THE KITCHEN
+            </h2>
+          </div>
+        </div>
+        <div className={styles.bottom}>
+          <p className={styles.error_msg}>
+            The page you are looking for might be removed or temporary
+            unavailable
+          </p>
+          <Link href="../" className={styles.return_btn}>
+            GO BACK TO HOME PAGE
+          </Link>
+        </div>
+      </section>
+    </>
+  );
 }
